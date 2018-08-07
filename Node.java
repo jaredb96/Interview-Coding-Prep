157d56384150f0707df5a01ec3e6f36091fd6ebc
jaredb96 <41237993+jaredb96@users.noreply.github.com>
2018-08-06 22:55:29 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,32 @@
+public class Node<T> {
+    private T value;
+    private Node next;
+
+    public Node(T val) {
+        value = val;
+        next = null;
+    }
+
+    public Node(T val, Node n) {
+        value = val;
+        next = n;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setValue(T val) {
+        value = val;
+    }
+
+    public void setNext(Node n) {
+        next = n;
+    }
+
+
+}
